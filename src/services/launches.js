4bd5ba6eb6ecef8c9e90cb9c67b2fc36f592b037
Esteban Velasquez <eavc.30@gmail.com>
2022-04-13 13:11:43 -0500

    feat(front): add launches service

---
+++ src/services/launches.js
@@ -0,0 +1,30 @@
+const API_URL = "https://api.spacexdata.com/v3";
+
+/**
+ * @description - This function is used to get all the launches
+ * @returns {Promise}
+ */
+export async function getAllLaunches() {
+  try {
+    const response = await fetch(`${API_URL}/launches`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+/**
+ * @description - This function is used to get launch by flight number
+ * @param {*} flightNumber
+ * @returns {Promise}
+ */
+export async function getLaunchByFlightNumber(flightNumber) {
+  try {
+    const response = await fetch(`${API_URL}/launches/${flightNumber}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
