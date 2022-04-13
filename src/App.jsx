e1acea925dd5db69ed7b8025979b89ea53d60917
Esteban Velasquez <eavc.30@gmail.com>
2022-04-13 13:32:45 -0500

    feat(front): consume spacex service from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,38 @@
+import { useState, useEffect } from "react";
+import * as API from "./services/launches";
+
 export function App() {
-  return <div>Hello World from React!</div>;
+  const [launches, setLaunches] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    API.getAllLaunches()
+      .then((data) => {
+        setLaunches(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>SpaceX Launches</h1>
+      <section>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <ul>
+            {launches.map((launch) => (
+              <li key={launch.flight_number}>
+                <h2>{launch.mission_name} ({launch.launch_year})</h2>
+                <p>{launch.details}</p>
+              </li>
+            ))}
+          </ul>
+        )}
+      </section>
+    </>
+  )
 }
