aac02da3cd7c1a82b7338e11e09a258de70e2a45
Esteban Velasquez <eavc.30@gmail.com>
2022-04-13 14:36:46 -0500

    feat(front): add style to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import { Box, Flex, Text, Spacer, Tag, Heading, Image } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
+import logo from "./assets/spacex-logo.png";
 import * as API from "./services/launches";
 
 export function App() {
@@ -18,21 +20,34 @@ export function App() {
 
   return (
     <>
-      <h1>SpaceX Launches</h1>
-      <section>
-        {loading ? (
+      <Image src={logo} width={300} m={4} alt="Spacex" />
+      <Heading align="center" as="h1" size="lg" m={4}>
+        SpaceX Launches
+      </Heading>
+              {loading ? (
           <p>Loading...</p>
         ) : (
-          <ul>
+          <section>
             {launches.map((launch) => (
-              <li key={launch.flight_number}>
-                <h2>{launch.mission_name} ({launch.launch_year})</h2>
-                <p>{launch.details}</p>
-              </li>
+              <Box
+                bg="gray.100"
+                p={4}
+                m={4}
+                borderRadius="lg"
+              >
+                <Flex>
+                  <Text fontSize="2xl">
+                    Mission <strong>{launch.mission_name}</strong> ({launch.launch_year})
+                  </Text>
+                  <Spacer />
+                  <Tag p={2} size="sm" colorScheme={launch.launch_success ? "green" : "red"}>
+                    {launch.launch_success ? "Success" : "Failure"}
+                  </Tag>
+                </Flex>
+              </Box>
             ))}
-          </ul>
+          </section>
         )}
-      </section>
     </>
-  )
+  );
 }
